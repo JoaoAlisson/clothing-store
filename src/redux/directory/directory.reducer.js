4ba1bf7230644636374dd9cc3806616dce455d10
João Alisson <joaoalissonsobral@gmail.com>
2020-04-02 19:09:24 -0300

    State normalization

--- src/redux/directory/directory.reducer.js
+++ src/redux/directory/directory.reducer.js
@@ -4,33 +4,33 @@ const INITIAL_STATE = {
             title: 'teste',
             imageUrl: 'http://i.ibb.co/R70vBrQ/menu.png',
             id: 1,
-            linkUrl: 'hats'
+            linkUrl: 'shop/hats'
         },
         {
             title: 'teste',
             imageUrl: 'http://i.ibb.co/R70vBrQ/menu.png',
             id: 2,
-            linkUrl: ''
+            linkUrl: 'shop/jackets'
         },
         {
             title: 'teste',
             imageUrl: 'http://i.ibb.co/R70vBrQ/menu.png',
             id: 3,
-            linkUrl: ''
+            linkUrl: 'shop/sneakers'
         },
         {
             title: 'teste',
             imageUrl: 'http://i.ibb.co/R70vBrQ/menu.png',
             size: 'large',
             id: 4,
-            linkUrl: ''
+            linkUrl: 'shop/womens'
         },
         {
             title: 'teste',
             imageUrl: 'http://i.ibb.co/R70vBrQ/menu.png',
             size: 'large',
             id: 5,
-            linkUrl: ''
+            linkUrl: 'shop/mens'
         },
     ]
 };
